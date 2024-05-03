aa258097c636a3f63a8d3327ed87aa00a461f43b
Ashna Razdan <65045279+ashnarazdan123@users.noreply.github.com>
2024-05-03 00:01:16 -0400

    all the getStats functions

--- Recommender.py
+++ Recommender.py
@@ -1,6 +1,8 @@
 import tkinter
 from Book import Book
 from Show import Show
+from Media import Media
+from tkinter import filedialog
 class Recommender():
     def __init__(self):
          self.__bookDict = {}
@@ -12,7 +14,7 @@ class Recommender():
         root.withdraw()
         
         while True:
-            path = tkinter.filedialog.askopenfilename(title = 'Select books file') 
+            path = tkinter.filedialog.askopenfilename(title='Select books file')
 
             if not path:
                 print("No file selected")
@@ -20,10 +22,14 @@ class Recommender():
                 try:
                     with open(path, 'r') as file:
                         for line in file:
-                            line = line.strip().split()
+                            line = line.strip().split(',')
+                            if len(line) < 11:
+                                print("Invalid format in line:", line)
+                                continue
                             newBook = Book(line[0], line[1], line[3], line[2], line[4], line[5], line[6], line[7], line[8], line[9], line[10])
                             self.__bookDict[newBook.getId()] = newBook
                     file.close()
+                    break
                 except FileNotFoundError:
                     print("File not found.")
                     continue
@@ -41,11 +47,12 @@ class Recommender():
                 try:
                     with open(path, 'r') as file:
                         for line in file:
-                            line = line.strip().split()
+                            line = line.strip().split(',')
                             #show_id,type,title,director,cast,average_rating,country,date_added,release_year,rating,duration,listed_in,description
                             newShow = Show(line[0], line[2], line[5], line[1], line[3], line[4], line[6], line[7], line[8], line[9], line[10], line[11], line[12])  
                             self.__showDict[newShow.getId()] = newShow
                     file.close()
+                    break
                 except FileNotFoundError:
                     print("File not found.")
                     continue
@@ -109,24 +116,198 @@ class Recommender():
         for item in self.__showDict:
             if item.getShow() == "TV Show":
                 print(item.getTitle(), item.getDuration())
+
     def getBookList(self):
         # Return Title and author of all books
         for item in self.__bookDict:
             print(item.getTitle(), item.getAuthors())
             
     def getMovieStats(self):
-        # Return Ratings, percentage of ratings, (2 decimals)
-        # Avg movie duration, (2 decimals)
-        # Director of most movies
-        # Actor of most movies
-        # Most frequent movie genre
+        ratingDict = {}
+        totalMovies = 0
+        directorsDict = {}
+        actorsDict = {}
+        genreDict = {}
+        ratingPercentages = {}
+        totalDuration = 0
+
+        for show in self.__showDict.values():
+            if show.getShow() == "Movie":
+                rating = show.getRating()
+                if rating not in ratingDict:
+                    ratingDict[rating] = 1
+                else:
+                    ratingDict[rating] += 1
+
+                duration = show.getDuration().split(" ")
+               
+                if len(duration) == 2 and "min" in duration[1]:
+                    try:
+                        totalDuration += int(duration[0])
+                        totalMovies += 1
+                    except ValueError:
+                        pass
+
+                director = show.getDirectors()
+                if director.strip():
+                    if director not in directorsDict:
+                        directorsDict[director] = 1
+                    else:
+                        directorsDict[director] += 1
+
+                actors = show.getActors().split("\\")
+                for actor in actors:
+                    if actor.strip():
+                        if actor not in actorsDict:
+                            actorsDict[actor] = 1
+                        else:
+                            actorsDict[actor] += 1
+
+                genres = show.getGenres().split(",")
+                for genre in genres:
+                    if genre.strip():
+                        if genre not in genreDict:
+                            genreDict[genre] = 1
+                        else:
+                            genreDict[genre] += 1
+
+        totalShows = sum(ratingDict.values())
+
+        for rating, count in ratingDict.items():
+            percentage = (count / totalShows) * 100
+            ratingPercentages[rating] = "{:.2f}".format(percentage)
+
+        if totalMovies > 0:
+            avgDuration = totalDuration / totalMovies
+        else:
+            avgDuration = 0
+
+        maxDirector = None
+        if directorsDict:
+            maxDirector = max(directorsDict, key=directorsDict.get)
+
+        maxActor = None
+        if actorsDict:
+            maxActor = max(actorsDict, key=actorsDict.get)
+
+        maxGenre = None
+        if genreDict:
+            maxGenre = max(genreDict, key=genreDict.get)
+
+        return ratingPercentages, "{:.2f}".format(avgDuration), maxDirector, maxActor, maxGenre
+
     def getTVStats(self):
-        
+        ratingDict = {}
+        totalSeasons = 0
+        actorsDict = {}
+        ratingPercentages = {}
+
+        for show in self.__showDict.values():
+            if show.getShow() == "TV Show":
+                rating = show.getRating()
+                if rating not in ratingDict:
+                    ratingDict[rating] = 1
+                else:
+                    ratingDict[rating] += 1
+
+                duration = show.getDuration().split(" ")
+                if len(duration) == 2 and "Season" in duration[1]:
+                    try:
+                        num_seasons = int(duration[0])
+                        totalSeasons += num_seasons
+                    except ValueError:
+                        pass
+
+                actors = show.getActors().split("\\")
+                for actor in actors:
+                    if actor.strip():
+                        if actor not in actorsDict:
+                            actorsDict[actor] = 1
+                        else:
+                            actorsDict[actor] += 1
+
+        totalShows = sum(ratingDict.values())
+       
+        for rating, count in ratingDict.items():
+            percentage = (count / totalShows) * 100
+            ratingPercentages[rating] = "{:.2f}".format(percentage)
+
+        if totalShows > 0:
+            avgSeasons = totalSeasons / totalShows
+        else:
+            avgSeasons = 0
+
+        maxActor = None
+        if actorsDict:
+            maxActor = max(actorsDict, key=actorsDict.get)
+
+        return ratingPercentages, "{:.2f}".format(avgSeasons), maxActor
+
+    
     def getBookStats(self):
+        pageCountTotal = 0
+        authorsDict = {}
+        publishersDict = {}
+
+        for book in self.__bookDict.values():
+            pageCount = book.getNumPages()
+            try:
+                pageCountTotal += int(pageCount)
+            except ValueError:
+                pass
+
+            authors = book.getAuthors().split("\\")  
+            for author in authors:
+                if author.strip():
+                    if author not in authorsDict:
+                        authorsDict[author] = 1
+                    else:
+                        authorsDict[author] += 1
+
+            publisher = book.getPublisher()  
+            if publisher:  
+                if publisher not in publishersDict:
+                    publishersDict[publisher] = 1
+                else:
+                    publishersDict[publisher] += 1
+
+
+        totalBooks = len(self.__bookDict)
+
+        if totalBooks > 0:
+            avgPageCount = pageCountTotal / totalBooks  
+        else:
+            avgPageCount = 0
+
+        maxAuthor = None
+        if authorsDict:
+            maxAuthor = max(authorsDict, key=authorsDict.get)
+
+        maxPublisher = None
+        if publishersDict:
+            maxPublisher = max(publishersDict, key=publishersDict.get)
+
+
+        return "{:.2f}".format(avgPageCount), maxAuthor, maxPublisher
+
+
         
-    def searchTVMovies(self):
+   ## def searchTVMovies(self):
         
-    def searchBooks(self):
+  ##  def searchBooks(self):
         
-    def getRecommendations(self):
-           
+   ## def getRecommendations(self):
+def main():
+    #just to test functions will delete later 
+    recommender = Recommender()
+
+   
+    recommender.loadBooks()
+    book_stats = recommender.getBookStats()
+    print("\nBook Stats:")
+    print("Average Page Count:", book_stats[0])
+    print("Most Frequent Author:", book_stats[1])
+    print("Most Frequent Publisher:", book_stats[2])
+
+if __name__ == "__main__":
+    main()
